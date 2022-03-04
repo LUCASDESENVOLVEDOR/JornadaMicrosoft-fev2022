6ae6f49d2c529a23e27997979964b31170f90f00
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2022-03-03 21:09:55 -0300

    projeto ejogos

---
+++ Backend e serviços na nuvem da Microsoft Azure/aula03/EJOGOS/Controllers/EquipeController.cs
@@ -0,0 +1,33 @@
+﻿using EJOGOS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EJOGOS.Controllers
+{
+    public class EquipeController : Controller
+    {
+        // IActionResult ele é apropriado para quando temos diversos tipos possiveis.
+
+        //        Os tipos ActionResult representam vários códigos de status HTTP.Alguns tipos de retorno comuns nessa categoria são BadRequestResult(400), NotFoundResult(404) e OkObjectResult(200).
+
+        //Codigos HTPP
+
+        //[https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status](https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status)
+
+
+        Equipe equipeModel = new Equipe();
+
+        public IActionResult Index()
+        {
+            ViewBag.Lucas = equipeModel.LerEquipes();
+
+
+            //ViewBag = RESERVA DE ESPACO PARA ARMAZENARMOS INFORMAÇOES PARA RECUPERAR NA VIEW
+
+            //A ViewBag tem a função de "carregar" informações do Controller para a View. 
+           
+
+
+            return View();
+        }
+    }
+}
---
+++ Backend e serviços na nuvem da Microsoft Azure/aula03/EJOGOS/Models/Equipe.cs
@@ -0,0 +1,82 @@
+﻿using EJOGOS.Interface;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EJOGOS.Models
+{
+
+    //: representa a herança;
+    //interface = contrato.
+    public class Equipe : EquipeBase, IEquipe
+    {
+        //construtor
+
+        //ctor TAB TAB
+        public Equipe()
+        {
+            //metodo da classe de herança;
+            CriarPastaOuArquivo(caminho);
+        }
+
+        //prop TAB TAB
+
+        //duplicar linha ctrl + D  
+
+        public int IdEquipe { get; set; }
+        public string nome { get; set; }
+        public string Imagem { get; set; }
+
+        //variavel de dados.
+
+        //private nivel de acesso.
+        // const = constante = a variavel nao pode mudar.
+
+        private const string caminho = "Database/equipe.csv";
+
+        private string Preparar(Equipe e)
+        {
+            return $"{e.IdEquipe};{e.nome};{e.Imagem}";
+        }
+
+        public void Criar(Equipe novaequipe)
+        {
+
+            //preparar a classe para retornar um texto.
+
+            //array de strings   =  na posicao 0 vai existir nossa linha.
+            string[] equipe_texto = { Preparar(novaequipe) };
+
+            //arquivo vamos adicionar novas linhas.
+            //caminho , string formato de array.
+            File.AppendAllLines(caminho, equipe_texto);
+        }
+
+        public List<Equipe> LerEquipes()
+        {
+            List<Equipe> listadeEquipes = new List<Equipe>();
+
+            //pegar o texto e adicionar na classe.
+
+            string[] linhas = File.ReadAllLines(caminho);
+
+            //adicionar a classe na lista
+            foreach (string item in linhas)
+            {
+                // 1;NOME DO TIME; CAMINHO DA IMAGEM
+
+                string[] linhaEquipe = item.Split(';');
+
+                Equipe equipeAtual = new Equipe();
+                equipeAtual.IdEquipe = int.Parse(linhaEquipe[0]);
+                equipeAtual.nome = linhaEquipe[1];
+                equipeAtual.Imagem = linhaEquipe[2];
+
+                listadeEquipes.Add(equipeAtual);
+            }
+
+            //devolver a lista;
+            return listadeEquipes;
+
+        }
+    }
+}
