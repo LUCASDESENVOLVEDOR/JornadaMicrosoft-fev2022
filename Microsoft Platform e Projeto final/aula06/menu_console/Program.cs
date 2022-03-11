553ed7d38687ad2ad59c5b1718899419a2c9f0d5
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2022-03-11 19:31:23 -0300

    menu console

---
+++ Microsoft Platform e Projeto final/aula06/menu_console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace menu_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //usuario vai informar o que o programa deve fazer.
+            //tera algumas opcoes de escolha.
+
+            bool continuar = true;       
+
+            //faça
+            do
+            {
+                Console.WriteLine(@" Escolha uma opção:
+                                     1 - Carro
+                                     2 - Moto
+                                     3 - Caminhão
+                                     4 - Cadastrar
+                                     5 - Sair");
+
+                string opcaoEscolhida = Console.ReadLine();
+                Console.Clear();
+
+                switch (opcaoEscolhida)
+                {
+                    case "1": Console.WriteLine("Você escolheu Carro"); break;
+                    case "2": Console.WriteLine("Você escolheu Moto"); break;
+                    case "3": Console.WriteLine("Você escolheu Caminhão"); break;
+                    case "4": Cadastrar(); break;
+                    case "5": continuar = false; break;
+                    default: Console.WriteLine("Escolha não válida."); break;
+                }
+            } 
+            
+            while (continuar); //enquanto for verdadeiro faça.          
+
+
+        }
+
+
+        public static void Cadastrar()
+        {
+            Console.WriteLine("Cadastro realizado");
+        }
+    }
+}
